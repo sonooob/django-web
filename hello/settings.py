7fe7626dc06c27d0338d7ed8ab99773188cc9997
sonooob <290328440@qq.com>
2016-02-24 16:29:06 +0800

    3

--- hello/settings.py
+++ hello/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'west.apps.WestConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'west'
+    
 ]
 
 MIDDLEWARE_CLASSES = [
